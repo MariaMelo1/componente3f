0694d0fc4600ca56ec1e36e5aa5c00362402a976
MariaMelo1 <duda13062007@gmail.com>
2025-04-02 10:18:45 -0300

    componente

---
+++ src/componentes/Etiqueta.jsx
@@ -0,0 +1,10 @@
+const Etiqueta = (props)=>{
+  var textoPadrao = "Texto Padrão da Etiqueta"
+    return(
+      <>
+         <p>{(props.texto != null) ? props.texto : textoPadrao}</p>
+      </>
+    )
+}
+
+export default Etiqueta
